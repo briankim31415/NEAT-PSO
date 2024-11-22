d3857f4c9d93b1b8d2b4d7b0aa2ffbb953ff1a9b
brian <briankim31415@gmail.com>
2024-11-21 23:13:12 -0600

    Basic CNN model with searchable params

---
+++ code/model.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Load config file variables
+from config_loader import load_config
+config = load_config()
+num_epochs = config['model_num_epochs']
+criterion_mapping = { int(k): getattr(torch.nn, v) for k, v in config['model_criterion_mapping'].items() }
+optimizer_mapping = { int(k): getattr(torch.optim, v) for k, v in config['model_optimizer_mapping'].items() }
+
+
+class Model(nn.Module):
+    def __init__(self, num_conv_layers, num_filters, dropout_rate):
+        super(Model, self).__init__()
+        self.conv_layers = nn.Sequential(
+            *[nn.Sequential(
+                nn.Conv2d(3 if i == 0 else num_filters, num_filters, kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2)
+            ) for i in range(num_conv_layers)]
+        )
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(num_filters * (32 // (2 ** num_conv_layers)) ** 2, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.fc(x)
+        return x
+
+
+class ModelHandler:
+    def __init__(self, particle: list):
+        # Get particle particle
+        num_conv_layers = particle[0]
+        num_filters = particle[1]
+        dropout_rate = particle[2]
+
+        self.lr = particle[3]
+        self.criterion_choice = criterion_mapping[int(particle[4])]
+        self.optimizer_choice = optimizer_mapping[int(particle[5])]
+
+        # Create particle model
+        self.model = Model(num_conv_layers, num_filters, dropout_rate)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+
+
+    def train(self, train_loader):
+        criterion = self.criterion_choice()
+        optimizer = self.optimizer_choice(self.model.parameters(), lr=self.lr)
+
+        self.model.train()
+        for epoch in range(num_epochs):
+            for images, labels in train_loader:
+                images, labels = images.to(self.device), labels.to(self.device)
+                optimizer.zero_grad()
+                outputs = self.model(images)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+    def evaluate(self, test_loader):
+        self.model.eval()
+        correct, total = 0, 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images, labels = images.to(self.device), labels.to(self.device)
+                outputs = self.model(images)
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        accuracy = correct / total
+        return accuracy
+    
+    def count_parameters(self):
+        total_params = sum(p.numel() for p in self.model.parameters())
+        return total_params
--- code/pso.py
+++ code/pso.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+from model import ModelHandler
 
 # Load config file variables
 from config_loader import load_config
@@ -8,15 +10,18 @@ num_iterations = config['PSO_num_iterations']
 w = config['PSO_w']
 c1 = config['PSO_c1']
 c2 = config['PSO_c2']
-dimensions_bounds = config['PSO_dimensions_bounds']
+continuous_bounds = config['model_continuous_bounds']
+criterion_bounds = [0, len(config['model_criterion_mapping']) - 1]
+optimizer_bounds = [0, len(config['model_optimizer_mapping']) - 1]
 
 # Extract dimensions and bounds
-dimension = [dim for dim in dimensions_bounds]
-num_dimensions = len(dimension)
-bounds = [b for b in dimensions_bounds.values()]
+dimensions = [dim for dim in continuous_bounds] + ['criterion', 'optimizer']
+num_dimensions = len(dimensions)
+bounds = [b for b in continuous_bounds.values()] + [criterion_bounds, optimizer_bounds]
 
 # Initialize particles
 particles = np.random.uniform([b[0] for b in bounds], [b[1] for b in bounds], (num_particles, num_dimensions))
+particles = np.array([clip_particle(p) for p in particles])
 velocities = np.zeros_like(particles)
 personal_best_positions = np.copy(particles)
 personal_best_scores = np.array([fitness_eval(p) for p in particles])
@@ -24,29 +29,51 @@ global_best_position = personal_best_positions[np.argmin(personal_best_scores)]
 global_best_score = np.min(personal_best_scores)
 
 
-def fitness_eval(particle):
-    num_conv_layers = particle[0]
-    num_filters = particle[1]
-    dropout_rate = particle[2]
-    # Run epochs of the arch with given position parameters
-    # Also involve complexity of the model (less complex is better)
-    # Return fitness score
-    return 1
 
+# Clip particle values
+def clip_particle(particle):
+    for i in range(len(particle)):
+        # Clip continuous parameters
+        particle[i] = np.clip(particle[i], bounds[i][0], bounds[i][1])
+
+        # Round values for discrete parameters (criterion and optimizer)
+        if i >= len(particle) - 2:
+            particle[i] = round(particle[i])
+    return particle
+
+
+# Fitness evaluation function
+def fitness_eval(particle):
+    # Create model with given particle parameters
+    model = ModelHandler(list(particle))
+
+    # Train model and run model to get accuracy
+    model.train(train_loader=None) # TODO Change this to the actual train loader
+    accuracy = model.evaluate(test_loader=None) # TODO Change this to the actual test loader
+
+    # Get complexity of the model (number of parameters)
+    complexity = model.count_parameters()
+
+    # Return fitness score (want to minimize, i.e. less is better)
+    return 1 - accuracy
+
+
+# PSO loop
 def run_pso():
-    # PSO Loop
+    # Go through every particle at each iteration of the PSO loop
     for iteration in range(num_iterations):
         for i in range(num_particles):
             # Update velocity
             r1, r2 = np.random.rand(), np.random.rand()
             velocities[i] = (
-                w * velocities[i]
-                + c1 * r1 * (personal_best_positions[i] - particles[i])
-                + c2 * r2 * (global_best_position - particles[i])
+                w * velocities[i] # Influence of the current velocity
+                + c1 * r1 * (personal_best_positions[i] - particles[i]) # Influence of the particle memory
+                + c2 * r2 * (global_best_position - particles[i]) # Influence of the swarm
             )
+
             # Update position
             particles[i] += velocities[i]
-            particles[i] = np.clip(particles[i], [b[0] for b in bounds], [b[1] for b in bounds])
+            particles[i] = clip_particle(particles[i])
 
             # Evaluate fitness
             fitness = fitness_eval(particles[i])
@@ -62,8 +89,8 @@ def run_pso():
             global_best_position = personal_best_positions[best_particle_idx]
             global_best_score = personal_best_scores[best_particle_idx]
 
-        # Print progress
-        print(f"Iteration {iteration+1}/{num_iterations}, Best Score: {global_best_score}")
-
     # Return best parameters
+    print("Best parameters")
+    for dim, val in zip(dimensions, global_best_position):
+        print(f"   {dim}: {val}")
     return global_best_position
